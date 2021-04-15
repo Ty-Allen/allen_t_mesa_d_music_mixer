764422fafebba5540ed5060cdfe3894a69c071fa
Tyler Allen <tyrules@hotmail.com>
2021-04-15 15:39:29 -0400

    audio now plays on drop

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@
         dragImages = document.querySelectorAll(".music-image"),
         dropZones = document.querySelectorAll(".drop-zone");
 
-
   //functions
   function dragStart(e) {
     console.log('started dragging');
     event.dataTransfer.setData("savedID", this.id);
+    event.dataTransfer.setData("savedData", this.dataset.audio);
   }
 
   function draggedOver(e) {
@@ -24,6 +24,13 @@
     let targetID = event.dataTransfer.getData("savedID");
     console.log("I dragged this image", targetID);
     event.target.appendChild(document.querySelector(`#${targetID}`));
+
+    let data = event.dataTransfer.getData("savedData");
+    console.log("Has Id", data);
+    let audio = document.querySelector(`audio[data-audio="${data}"]`);
+
+    audio.play();
+    audio.loop = true;
   }
 
   //event handling
